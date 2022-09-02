7c899dc02d1b4dfa44854d1065e87a630d6966c9
Sophia Kerber Shigueoka <sophia.shigueoka@gmail.com>
2022-09-02 12:49:03 -0300

    Network info done

--- main.py
+++ main.py
@@ -4,23 +4,31 @@ import socket
 import os
 import netifaces
 from rich import print as rprint
+from rich import box
 from rich.console import Console, group
 from rich.prompt import Prompt
 from rich.panel import Panel
+from rich.padding import Padding
+from rich.table import Table
+from rich.prompt import Confirm
 
 debug = True
 
 __author__ = "Sophia Kerber"
 __license__ = "MIT"
 
-predictable_iface_names = ['eth', 'en', 'wl']
+out_interfaces = dict()
+out_addrs = dict()
+out_network = dict()
 
-iface_to_ip_dict = {
-    'eth': False,
-    'en': False,
-    'wl': True,
-    'ww': True
-}
+# predictable_iface_names = ['eth', 'en', 'wl']
+
+# iface_to_ip_dict = {
+#     'eth': False,
+#     'en': False,
+#     'wl': True,
+#     'ww': True
+# }
 
 
 console = Console()
@@ -34,7 +42,6 @@ def get_gateways():
     gws = netifaces.gateways()
     for gw in gws:
         try:
-            print(gws)
             gw_iface = gws[gw][netifaces.AF_INET]
             gw_ip, iface = gw_iface[0], gw_iface[1]
             gw_list = [gw_ip, iface]
@@ -45,44 +52,75 @@ def get_gateways():
 
 def get_addresses(iface):
     addrs = netifaces.ifaddresses(iface)
+    
+    # AF LINK -> link layer interface (ethernet)
     link_addr = addrs[netifaces.AF_LINK]
-    iface_addrs = addrs[netifaces.AF_INET]
-    iface_dict = iface_addrs[0]
-    link_dict = link_addr[0]
-    hwaddr = link_dict.get('addr')
-    iface_addr = iface_dict.get('addr')
-    iface_broadcast = iface_dict.get('broadcast')
-    iface_netmask = iface_dict.get('netmask')
+    if netifaces.AF_INET in addrs.keys():
+        # AF_INET -> normal internet addresses (IPV4)
+        out_addrs["ipv4"] = addrs[netifaces.AF_INET]
+        # AF_INET6 -> PIV6 internet addresses
+        if netifaces.AF_INET6 in addrs.keys():
+            out_addrs["ipv6"] = addrs[netifaces.AF_INET6]
+        out_interfaces[iface] = out_addrs
+
+    hwaddr = link_addr[0].get('addr')
+    iface_addr = out_addrs["ipv4"][0].get('addr')
+    iface_broadcast = out_addrs["ipv4"][0].get('broadcast')
+    iface_netmask = out_addrs["ipv4"][0].get('netmask')
     return hwaddr, iface_addr, iface_broadcast, iface_netmask
-
-
 
 def help():
     pass
 
-@group()
-def ui():
-    console.rule("[bold red]SOPA'S PORT SCANNER", style="bold red")
+def exploit():
+    pass
 
+def network():
+    pass
+
+def network_info():
+    interfaces = get_interfaces()
+    gws = get_gateways()
+    for iface in interfaces:
+        addr = get_addresses(iface)
+        out_network[iface] = addr
+    
+    print("{:<15} {:<25} {:<15} {:<15} {:<15}".format('Interface','MAC Address','IP Address', 'Broadcast', 'Netmask'))
+    print("-"*85)
+    for k, v in out_network.items():
+        mac, ip, broad, netmask = v
+        if (broad is None):
+            broad = "None"
+        print("{:<15} {:<25} {:<15} {:<15} {:<15}".format(k, mac, ip, broad, netmask))
+    print("\n")
+        
+
+def ui():
+    rprint("\n")
+    console.rule("[bold red]SOPA'S PORT SCANNER", style="bold red")
+    
+    rprint(Padding("[DISCLAIMER]\nUse at your own risk", (2, 4), style='light_sea_green', expand=True))
     if not debug:
         fastscan = Prompt.ask("Would you like to do a fast system scan?", choices=["Y", "n"])
     fastscan = "Y"
 
     if fastscan == "Y":
-        interfaces = get_interfaces()
-        gws = get_gateways()
-        for iface in interfaces:
-            addr = get_addresses(iface)
-            yield Panel(addr, style="on blue")
-            
-
-        yield Panel(interfaces, style="on red")
-        yield Panel(gws, style="on blue")
-
+        network_info()
+        options = ["q", "e", "n"]
+        next = Prompt.ask("What next? [Quit (q), Exploitation (e), Network (n)]")
+        while next not in options:
+            if next == "q":
+                quit()
+            if next == "e":
+                exploit()
+            if next == "n":
+                network()
+            else:
+                print("Please choose one of the options")
+                next = Prompt.ask("What next? [Quit (q), Exploitation (e), Network (n)]")
+        knowledgeable = Confirm.ask("If you choose this option, you are liable for your own actions. \nBeware of the networks you are exploiting: you must have previous consent of the system/network/app admin. \nWould you like to proceed?")
+        assert knowledgeable
     pass
-
-
-print(Panel(ui()))
 
 
 
